c0211e79b85b894b651cd23d38ee03b8c7b32215
Michael Giuliana <michael.giuliana@gmail.com>
2012-04-19 10:28:22 -0400

    js script back in display.php

--- widgets/slider/views/display.php
+++ widgets/slider/views/display.php
@@ -1,11 +1,8 @@
-<div class="slider-wrapper theme-default">
+<div class="slider-wrapper">
 	<div id="slider_<?php echo $slider->id; ?>" class="nivoSlider">
 		<?php foreach($images as $image): ?>
-		<img src="<?php echo $image->path; ?>" alt="" />
+		<img src="<?php echo $image->path; ?>" alt="Cool." />
 		<?php endforeach; ?>
-	</div>
-	<div id="htmlcaption" class="nivo-html-caption">
-		<strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
 	</div>
 </div>
 <script type="text/javascript">
