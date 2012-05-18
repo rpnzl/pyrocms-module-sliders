6eb86474c4320a11be3a1b7dd352f4f678990e4e
Michael Giuliana <michael.giuliana@gmail.com>
2012-05-18 18:52:39 -0400

    extended get_settings method for single fields

--- models/slider_m.php
+++ models/slider_m.php
@@ -1,17 +1,37 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 /**
- * Regular slider model
- *
+ * Slider Model
+ * 
  * @author Michael Giuliana
- *
  */
 class Slider_m extends MY_Model
 {
-	public function get_settings()
+	/**
+	 * Get Sliders Module settings.
+	 * 
+	 * Takes the work out of grabbing the settings whenever
+	 * we need them. Will also return a single value if
+	 * needed.
+	 * 
+	 * @param str $field The settings field to retrieve.
+	 * @return array / str
+	 */
+	public function get_settings($field = null)
 	{
-		$settings = parent::get_all();
-		$settings = $settings[0];
+		// return value of a single setting
+		if($field)
+		{
+			$settings = parent::get_all();
+			$settings = $settings[0];
+			$result = (property_exists($settings, $field)) ? $settings->{$field} : 'Invalid field.';
+		}
+		// return all settings
+		else
+		{
+			$settings = parent::get_all();
+			$result = $settings[0];
+		}
 
-		return $settings;
+		return $result;
 	}
 }
