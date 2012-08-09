ac81b2779d3149a23a344a9be49b70597387b339
Bojan Mazej <bojan.mazej@gmail.com>
2012-08-09 10:42:56 +0200

    Added Slovenian language

---
+++ language/slovenian/sliders_lang.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['sliders.jquery_label']				= 'Dodaj jQuery?';
+$lang['sliders.folder_id_label']			= 'Mapa za Modul';
+
+// titles
+$lang['sliders.create_title']				= 'Dodaj Slider';
+$lang['sliders.edit_title']				= 'Uredi Slider "%s"';
+$lang['sliders.list_title']				= 'Sliders';
+$lang['sliders.settings_title']				= 'Nastavitve';
+
+// messages
+$lang['sliders.settings_explanation_title']		= 'Pojasnilo';
+$lang['sliders.settings_explanation']			= 'To so osnovne nastavitve za Slider.<br><br><strong>Mapa za Modul</strong> se navezuje na mapo v <a href="/admin/files">Modulu Files</a> v katero se shranjujejo mape za posamezne sliderje. Ob instalaciji modula se ustvari privzeta mapa z imenom <em>Sliders Module</em>.<br><br><strong>Dodaj jQuery</strong> se uporabi v primeru, kadar le ta ni vključen v uporabljeno predlogo.';
